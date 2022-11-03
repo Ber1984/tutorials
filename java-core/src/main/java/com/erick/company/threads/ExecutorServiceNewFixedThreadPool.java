0008b16441f40c162b1a5e59ef6282aaf8794675
erickjhormanromerojojoa <erickjhorman@gmail.com>
2022-11-02 23:08:00 -0500

    add executor service fix thread pool class

---
+++ java-core/src/main/java/com/erick/company/threads/ExecutorServiceNewFixedThreadPool.java
@@ -0,0 +1,20 @@
+package com.erick.company.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceNewFixedThreadPool {
+    public static void main(String[] args) throws InterruptedException {
+        int threadNum = Integer.parseInt(args[0]);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
+
+        for (int j = 0; j < 20000; j++) {
+            executorService.execute(new Thread(() -> {
+                //Do something
+            }));
+        }
+        executorService.shutdown(); //prevents clients to send more work to the executor service.
+        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+    }
+}
